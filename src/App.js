0ba274ee5714ce7f297b9824061080543146e7d5
pritiprajapati314 <pritiprajapati314@gmail.com>
2021-09-09 10:11:29 +0530

    "changes"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './component/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './component/pages/home'
+import Login from './component/pages/userPage'
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Switch>
         <Route path='/' exact component = {Home} />
+        <Route path='/login' exact component = {Login} />
       </Switch>
     </Router>
       
