18c823bc70ba078392beab6dce83029ab35be2ec
pritiprajapati314 <pritiprajapati314@gmail.com>
2021-09-14 17:12:39 +0530

    "integrated the register page and few changes in login page"

--- src/component/Login/loginPage.js
+++ src/component/Login/loginPage.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import './loginPage.css'
+import style from './loginPage.module.css'
 
 function loginPage() {
     return (
-        <div id = "hero">
-            <div className= "container" >
-                <div className = "card">
-                    <div className = "inner-box">
-                        <div className = "card-front">
+        <div id = {style.hero}>
+            <div className= {style.container} >
+                <div className = {style.card}>
+                    <div className = {style.innerbox}>
+                        <div className = {style.cardfront}>
                             <h2>Login</h2>
                             <form>
-                                <input type="email" className="input-box" placeholder="&#xf199; Email Id" required />
-                                <input type="password" className="input-box" placeholder="&#xf09c; Password" required />
+                                <input type="email" className={style.inputbox} placeholder="&#xf199; Email Id" required />
+                                <input type="password" className= {style.inputbox} placeholder="&#xf09c; Password" required />
                             
-                                <button type="submit" className ="sub-btn">Submit</button>
+                                <button type="submit" className = {style.subbtn} >Submit</button>
                                 <input type="checkbox"/>
                                 <a href="/#">Forgot Password?</a><br/>
                             </form>
--- src/component/Register/register.js
+++ src/component/Register/register.js
@@ -1,57 +1,92 @@
 import React from 'react'
-import './style2.css'
+import style from './style2.module.css'
 
 function register() {
     return (
-        <div className="hero">
-            <div className="container">
-            <div className = "form-box">
-            <div className="title">Registration Form</div>
+        <div className={style.hero}>
+            <div className={style.container}>
+            <div className = {style.formbox}>
+            <div className={style.title}>Registration Form</div>
                 <form action="#">
-                    <div className="user-details">
-                        <div className="input-box">
-                            <span className="details">Full Name</span>
-                            <input type="text" className="icon"  placeholder="&#xf508; Enter your Name" required />
+                    <div className={style.userdetails}>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>Full Name</span>
+                            <input type="text" className={style.icon}  placeholder="&#xf508; Enter your Name" required />
                                         
                         </div>
-                        <div className="input-box">
-                            <span className="details">Vehicle Number</span>
-                            <input type="text"  className="icon" placeholder="&#xf63c; MP-04-HA-XXXX" required />
+                        <div className={style.inputbox}>
+                            <span className={style.details}>Vehicle Number</span>
+                            <input type="text"  className={style.icon} placeholder="&#xf63c; MP-04-HA-XXXX" required />
                                         
                         </div>
-                        <div className="input-box">
-                            <span className="details">Email Id</span>
-                            <input type="email"  className="icon" placeholder="&#xf199; Enter your Email Address" required />
+                        <div className={style.inputbox}>
+                            <span className={style.details}>Email Id</span>
+                            <input type="email"  className={style.icon} placeholder="&#xf199; Enter your Email Address" required />
                                         
                         </div>
-                        <div className="input-box">
-                            <span className="details">Contact Number</span>
-                            <input type="text"  className="icon" placeholder="&#xf879; 97XXXXXXX9" pattern="[0-9]{10}" required />
+                        <div className={style.inputbox}>
+                            <span className={style.details}>Contact Number</span>
+                            <input type="text"  className={style.icon} placeholder="&#xf879; 97XXXXXXX9" pattern="[0-9]{10}" required />          
+                        </div>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>Password</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Enter your Password" required />
                                         
                         </div>
-                        <div className="input-box">
-                            <span className="details">Password</span>
-                            <input type="password" className="icon"  placeholder="&#xf084; Enter your Password" required />
+                        <div className={style.inputbox}>
+                            <span className={style.details}>Confirm Password</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Confirm your password" required />                 
+                        </div>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>liscence no</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Enter your Password" required />
                                         
                         </div>
-                        <div className="input-box">
-                            <span className="details">Confirm Password</span>
-                            <input type="password" className="icon"  placeholder="&#xf084; Confirm your password" required />                 
+                        <div className={style.inputbox}>
+                            <span className={style.details}>alternative no</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Confirm your password" required />                 
+                        </div>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>dob</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Enter your Password" required />
+                                        
+                        </div>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>blood group</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Confirm your password" required />                 
+                        </div>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>address</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Enter your Password" required />
+                                        
+                        </div>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>pin</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Confirm your password" required />                 
+                        </div>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>district</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Enter your Password" required />
+                                        
+                        </div>
+                        <div className={style.inputbox}>
+                            <span className={style.details}>city</span>
+                            <input type="password" className={style.icon}  placeholder="&#xf084; Confirm your password" required />                 
                         </div>
                     </div>
-                    <div className="gender-details">
-                            Gender:
-                        
-                            Male <input type="radio" name="gender" id="male" />
-                            Female <input type="radio" name="gender" id="female" />
-                            Others<input type="radio" name="gender" id="other" />
+                    <div className={style.genderdetails}>
+                            Gender :
+                                
+                            <span>MALE <input type="radio" name="gender" id="male" /></span>
+                            <span>FEMALE <input type="radio" name="gender" id="female" /></span>
+                            <span>OTHERS<input type="radio" name="gender" id="other" /></span>
                     </div>
-                    <div className="button">
+                    <div className={style.button}>
                     <input type="submit" value="Register" />
                     </div>
                     <hr />
                     <h4>Already Registered?</h4>
-                    <a href="User_login.html"><div className="submit-btn">Login</div></a>
+                    <a href="User_login.html"><div className={style.submitbtn}>Login</div></a>
                 </form>
             </div>
                 
