8ed2e068ad6052ace9a89fdf67dadd5ac1f6051c
theshadycoder <kritikasinha2000@gmail.com>
2021-10-11 16:35:11 +0530

    manager request backend

--- src/component/pages/userPage.js
+++ src/component/pages/userPage.js
@@ -3,11 +3,6 @@ import Information from '../UserStyles/Information';
 import Footer from '../FrontPage/Footer';
 
 import Top from '../UserStyles/Top';
-<<<<<<< HEAD
-
-=======
-/* import Maps  from '../Maps'; */
->>>>>>> bf7708c99c3d0528c6ab4dd5b5e3e1fdcd8f4376
 import Navbar2 from '../UserStyles/Navbar2';
 
 function UserPage(){
